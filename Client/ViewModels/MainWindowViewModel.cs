8c999d8c2071b19d8532c7f0d55cb7e3293884e0
Evgeny Vyalyy <evyalyy@scorewarrior.com>
2023-03-25 20:15:10 +0200

    add Avalonia template for client project

---
+++ Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BasicMvvmSample.ViewModels;
+
+namespace Client.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+
+    // Add our RactiveViewModel
+    public ReactiveViewModel ReactiveViewModel { get; } = new ReactiveViewModel();
+}
